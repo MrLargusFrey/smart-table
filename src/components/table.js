4af444dd49e10676cf2e25b9dccd132c9902b3be
Alexander Filipyev <MrLargusFrey@yandex.ru>
2026-04-12 22:22:47 +0500

    готовый проект

--- src/components/table.js
+++ src/components/table.js
@@ -1,4 +1,4 @@
-import {cloneTemplate} from "../lib/utils.js";
+import { cloneTemplate } from "../lib/utils.js";
 
 /**
  * Инициализирует таблицу и вызывает коллбэк при любых изменениях и нажатиях на кнопки
@@ -8,18 +8,52 @@ import {cloneTemplate} from "../lib/utils.js";
  * @returns {{container: Node, elements: *, render: render}}
  */
 export function initTable(settings, onAction) {
-    const {tableTemplate, rowTemplate, before, after} = settings;
+    const { tableTemplate, rowTemplate, before, after } = settings;
     const root = cloneTemplate(tableTemplate);
 
     // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+    before.reverse().forEach(subName => {                            // перебираем нужный массив идентификаторов
+        root[subName] = cloneTemplate(subName);            // клонируем и получаем объект, сохраняем в таблице
+        root.container.prepend(root[subName].container);    // добавляем к таблице после (append) или до (prepend)
+    });
+
+    after.forEach(subName => {
+        root[subName] = cloneTemplate(subName);
+        root.container.append(root[subName].container);
+    });
 
     // @todo: #1.3 —  обработать события и вызвать onAction()
+    // Обработчик события change
+    root.container.addEventListener('change', () => {
+        onAction();
+    });
+
+    // Обработчик события reset
+    root.container.addEventListener('reset', () => {
+        setTimeout(() => {
+            onAction();
+        }, 0); // минимальная задержка для отложенного вызова
+    });
+
+    // Обработчик события submit
+    root.container.addEventListener('submit', (e) => {
+        e.preventDefault(); // предотвращаем стандартное поведение формы
+        onAction(e.submitter); // передаём сабмиттер в onAction
+    });
 
     const render = (data) => {
         // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
+        const nextRows = data.map(item => {
+            const row = cloneTemplate(rowTemplate)
+            Object.keys(item).forEach(key => {
+                if (row.elements[key]) {
+                    row.elements[key].textContent = item[key];
+                }
+            });
+            return row.container;
+        });
         root.elements.rows.replaceChildren(...nextRows);
     }
 
-    return {...root, render};
+    return { ...root, render };
 }
