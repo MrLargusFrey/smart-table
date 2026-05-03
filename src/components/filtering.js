971bfc65f34ae4843ed455476acdb8792a652f6e
Alexander Filipyev <MrLargusFrey@yandex.ru>
2026-05-03 22:28:29 +0500

    готовый проект

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,5 +1,3 @@
-// import { createComparison, defaultRules } from "../lib/compare.js";
-
 export function initFiltering(elements) {
     // @todo: #4.1 — заполнить выпадающие списки опциями
     const updateIndexes = (elements, indexes) => {
@@ -12,29 +10,41 @@ export function initFiltering(elements) {
             }))
         })
     }
+
+    // @todo: #4.3 — настроить компаратор
     const applyFiltering = (query, state, action) => {
-        // @todo: #4.5 — отфильтровать данные
+
+
+        // @todo: #4.2 — обработать очистку поля
+        if (action && action.name === 'clear') {
+            const field = action.dataset.field; // берём имя поля из атрибута data-field кнопки
+            const parent = action.closest('div'); // находим родительский элемент кнопки
+
+            // ищем input в родительском элементе и сбрасываем его значение
+            const input = parent.querySelector('input');
+            if (input) {
+                input.value = ''; // очищаем значение в UI
+            }
+
+            // сбрасываем значение в состоянии
+            state[field] = '';
+        }
+
+        // @todo: #4.5 — отфильтровать данные используя компаратор
         const filter = {};
         Object.keys(elements).forEach(key => {
-            if (elements[key] && ['INPUT', 'SELECT'].includes(elements[key].tagName) && elements[key].value) {
-                filter[`filter[${elements[key].name}]`] = elements[key].value;
+            if (elements[key]) {
+                if (['INPUT', 'SELECT'].includes(elements[key].tagName) && elements[key].value) { // ищем поля ввода в фильтре с непустыми данными
+                    filter[`filter[${elements[key].name}]`] = elements[key].value; // чтобы сформировать в query вложенный объект фильтра
+                }
             }
         })
 
-        return Object.keys(filter).length ? Object.assign({}, query, filter) : query;
+        return Object.keys(filter).length ? Object.assign({}, query, filter) : query; // если в фильтре что-то добавилось, применим к запросу
     }
 
     return {
         updateIndexes,
         applyFiltering
-    };
-}
-//     // @todo: #4.3 — настроить компаратор
-//     const compare = createComparison(defaultRules);
-
-//     return (data, state, action) => {
-
-//         // @todo: #4.5 — отфильтровать данные используя компаратор
-//         return data.filter(row => compare(row, state));
-//     };
-// }
+    }
+} 
